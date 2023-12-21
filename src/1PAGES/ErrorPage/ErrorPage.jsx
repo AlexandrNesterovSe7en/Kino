63f9568e5d922a42abe627c1fd2730ec1b450f22
Alexandr Nesterov <pudj.nesterov2004@mail.ru>
2023-12-21 18:02:25 +0800

    add Error Page 404

---
+++ src/1PAGES/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+    return (
+        <div>
+            Error Page 404            
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,13 @@
 import { Route, createHashRouter, createRoutesFromElements } from 'react-router-dom';
 import MainPage from './1PAGES/MainPage/MainPage';
+import ErrorPage from './1PAGES/ErrorPage/ErrorPage';
 const routes = createHashRouter(
     createRoutesFromElements(
       <Route 
         path='/'
-        element={<MainPage />}>
+        element={<MainPage />}
+        errorElement={<ErrorPage />}
+        >
       </Route>
     )
 )
