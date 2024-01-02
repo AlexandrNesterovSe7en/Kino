ec245021116966bd9e38e2021f846c28742a055b
kotl1n_ <ketkette7@gmail.com>
2024-01-02 17:40:18 +0300

    Немного изменил карточки

--- src/3COMPONENTS/Card/Card.jsx
+++ src/3COMPONENTS/Card/Card.jsx
@@ -6,7 +6,9 @@ import cl from "./Card.module.css"
 const Card = ({ img, title }) => {
     return ( 
         <div className={cl.cardWrapper}>
-            <img src={img} alt="card-img" className={cl.img}/>
+            <div className={cl.imgWrapper}>
+                <img src={img} alt="card-img" className={cl.img}/>
+            </div>
             <div className={cl.cardDescWrapper}>
                 <h4>{title}</h4>
                 <p className={cl.inSubscribe}>В подписке</p>
