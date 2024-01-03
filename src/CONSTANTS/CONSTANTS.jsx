3e72c9d0ecace4c6483ee37e00be5ea318ab5770
kotl1n_ <ketkette7@gmail.com>
2024-01-03 23:02:37 +0300

    Добавил немного данных и еще один переводчик

--- src/CONSTANTS/CONSTANTS.jsx
+++ src/CONSTANTS/CONSTANTS.jsx
@@ -2,11 +2,15 @@
 export const TRANSLATOR_RU_TO_EN = {
     "Сериалы": "Serials",
     "Боевики": "Fighters",
-    "Комедии": "Сomedies"
+    "Комедии": "Сomedies",
+    "Детям": "ForChildren",
+    "Бесплатно": "Free"
 }
 
 export const TRANSLATOR_EN_TO_RU = {
     "Serials": "Сериалы",
     "Fighters": "Боевики",
-    "Сomedies": "Комедии"
+    "Сomedies": "Комедии",
+    "ForChildren": "Детям",
+    "Free": "Бесплатно"
 }
