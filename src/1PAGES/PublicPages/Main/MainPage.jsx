afb78a41efe98a6f75633c22d2599b7fa55c816c
Alexandr Nesterov <pudj.nesterov2004@mail.ru>
2024-01-06 02:02:57 +0800

    add toggle modal for main page

--- src/1PAGES/PublicPages/Main/MainPage.jsx
+++ src/1PAGES/PublicPages/Main/MainPage.jsx
@@ -1,6 +1,5 @@
 import Header from "../../../2MODULES/Header/Header";
 import MainForFirstPage from "../../../2MODULES/MainForFirstPage/MainForFirstPage";
-import MainModal from "../../../2MODULES/Modal/MainModal/MainModal";
 
 
 const MainPage = () => {
@@ -10,7 +9,6 @@ const MainPage = () => {
         <>
             <Header />
             <MainForFirstPage />
-            <MainModal text="Спасибо за подписку!"/>
         </>
     );
 };
