7efe20fb7378690ac90a9744fd7adf7b5381869d
Alexandr Nesterov <pudj.nesterov2004@mail.ru>
2024-01-14 03:57:36 +0800

    Updates

--- static/js/472.dd8a92da.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkkinopoisk=self.webpackChunkkinopoisk||[]).push([[472],{472:(e,s,i)=>{i.r(s),i.d(s,{default:()=>a});var k=i(184);const a=()=>(0,k.jsx)("div",{children:"FavoritePage"})}}]);
-//# sourceMappingURL=472.dd8a92da.chunk.js.map
